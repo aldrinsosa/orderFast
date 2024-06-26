74ed7e4adbfa5ed98cb4bed7aed07a798cb0f872
Aldrin Sosa <sosaadrin2006@gmail.com>
2024-06-26 00:44:29 -0500

    fix font not loading and creating directory for the ttf file

--- src/main/java/com/sosaco/orderfast/Main.java
+++ src/main/java/com/sosaco/orderfast/Main.java
@@ -4,15 +4,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("views/scenes/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        Font.loadFont(Objects.requireNonNull(getClass().getResource("fonts/Alatsi.ttf")).toExternalForm(), 13);
 
         stage.setTitle("OrderFast");
         Image icon = new Image("icon.png");
