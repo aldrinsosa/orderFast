57f43b4740de29571b65ce8f5000f9b5c41f088c
Aldrin Sosa <sosaadrin2006@gmail.com>
2024-06-28 22:44:06 -0500

    create order Item component

---
+++ src/main/java/com/sosaco/orderfast/controllers/components/OrderItem.java
@@ -0,0 +1,50 @@
+package com.sosaco.orderfast.controllers.components;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+
+import java.io.IOException;
+
+public class OrderItem extends HBox {
+
+    @FXML
+    private Text recipeText;
+
+    @FXML
+    private Label amountLabel;
+
+    private String recipe;
+    private int amount;
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+        amountLabel.setText(Integer.toString(amount));
+    }
+
+    public String getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(String recipe) {
+        this.recipe = recipe;
+        recipeText.setText(recipe);
+    }
+
+    public OrderItem() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/sosaco/orderfast/views/components/order-item.fxml"));
+        loader.setRoot(this);
+        loader.setController(this);
+        try {
+            loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
