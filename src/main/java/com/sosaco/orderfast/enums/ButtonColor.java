b6459f360c3b2714feeee3f1d418c574e4d06053
Aldrin Sosa <sosaadrin2006@gmail.com>
2024-07-03 16:15:23 -0500

    set the icon and color enum with the values integrated in them

--- src/main/java/com/sosaco/orderfast/enums/ButtonColor.java
+++ src/main/java/com/sosaco/orderfast/enums/ButtonColor.java
@@ -1,10 +1,20 @@
 package com.sosaco.orderfast.enums;
 
 public enum ButtonColor {
-    RED,
-    GREEN,
-    YELLOW,
-    BROWN,
-    MUSTARD,
-    MINT
+    RED ("#E26D5C"),
+    GREEN ("#C9CBA3"),
+    YELLOW ("#FFE1A8"),
+    BROWN ("#754F44"),
+    MUSTARD ("#FFCF56"),
+    MINT ("#86BAA1");
+
+    private final String rgbColor;
+
+    ButtonColor(String rgbColor) {
+        this.rgbColor = rgbColor;
+    }
+
+    public String getRgbColor() {
+        return rgbColor;
+    }
 }
